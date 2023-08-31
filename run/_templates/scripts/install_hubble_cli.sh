bd5e9d58784f542b9ac39b89c06b2f2bb8bbc359
Steven Lee <steven.lee@kynrai.com>
2023-08-31 12:00:15 +0100

    Remove cilium and k8s

--- run/_templates/scripts/install_hubble_cli.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export HUBBLE_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/hubble/master/stable.txt)
-HUBBLE_ARCH=amd64
-if [ "$(uname -m)" = "arm64" ]; then HUBBLE_ARCH=arm64; fi
-curl -L --fail --remote-name-all https://github.com/cilium/hubble/releases/download/$HUBBLE_VERSION/hubble-darwin-${HUBBLE_ARCH}.tar.gz{,.sha256sum}
-shasum -a 256 -c hubble-darwin-${HUBBLE_ARCH}.tar.gz.sha256sum
-sudo tar xzvfC hubble-darwin-${HUBBLE_ARCH}.tar.gz /usr/local/bin
-rm hubble-darwin-${HUBBLE_ARCH}.tar.gz{,.sha256sum}
